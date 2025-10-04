f4589185bcc1a02434a63348f9c6d26a54b570d6
NELSON PARAISO <paraisonelson@gmail.com>
2025-10-04 18:04:28 +0200

    remove helmet

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+
 import ScrollToTop from "./Components/ScrollToTop"; // 👈 ici
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
