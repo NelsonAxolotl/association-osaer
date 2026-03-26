be030c274f28c7512a59ee7843344aa775ea9c9a
NELSON PARAISO <paraisonelson@gmail.com>
2026-03-26 10:37:00 +0100

    finishing glow

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,7 +18,16 @@ const Footer = forwardRef((props, externalRef) => {
       ref={externalRef}
     >
       <div className="footer-content">
-        <p className="footer-copy">&copy; 2026 OSAER — The Cool Axolotl</p>
+        <p className="footer-copy">
+          &copy; 2026 OSAER —{" "}
+          <a
+            href="https://www.thecoolaxolotl.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            The Cool Axolotl
+          </a>
+        </p>
         <p className="footer-legal">
           <Link to="/mentions-politique">
             Mentions légales & Politique de confidentialité
